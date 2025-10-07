378ab87814594397b7bbd4d4b6e1bfd9b421be38
lucas <lucas.escaffre@ynov.com>
2025-10-07 14:11:04 +0200

    Refactor homepage to include user authentication and display quiz categories

--- src/app/quiz/[category]/page.tsx
+++ src/app/quiz/[category]/page.tsx
@@ -1,3 +1,11 @@
-export default function Page() {
-    return <h1>Hello, Blog Post Page!</h1>
+"use client"
+
+import {use} from "react";
+
+export default function QuizPage({ params }: { params: Promise<{ category: string }> }) {
+    const { category } = use(params);
+
+
+
+    return <h1>Quiz Category: {category}</h1>;
 }
